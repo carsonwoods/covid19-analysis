d155c24c634bca563b52f71ec69511f3db6b3416
Carson Woods <cwoods289@gmail.com>
2020-11-23 18:47:02 +0000

    Improve count logic

--- analysis.py
+++ analysis.py
@@ -431,27 +431,29 @@ driving_average = 0
 walking_average = 0
 residential_average = 0
 workplace_average = 0
-count = 0
+apple_count = 0
+google_count = 0
 
 for root, subdirs, files in os.walk('./results/'):
     for file in files:
         if file.endswith("regression_performance.txt"):
-            count += 1
             with open(os.path.join(root, file), "r") as fp:
                 line = fp.readline()
                 while line:
                     line = line.split()
                     if 'Driving' in line:
+                        apple_count += 1
                         driving_average += float(line[-1])
                     elif 'Walking' in line:
                         walking_average += float(line[-1])
                     elif 'Residential' in line:
+                        google_count += 1
                         residential_average += float(line[-1])
                     elif 'Workplace' in line:
                         workplace_average += float(line[-1])
                     line = fp.readline()
 
-print("\nDriving Average: " + str(driving_average/count))
-print("Walking Average: " + str(walking_average/count))
-print("Residential Average: " + str(residential_average/count))
-print("Workplace Average: " + str(workplace_average/count))
+print("\nDriving Average: " + str(driving_average/apple_count))
+print("Walking Average: " + str(walking_average/apple_count))
+print("Residential Average: " + str(residential_average/google_count))
+print("Workplace Average: " + str(workplace_average/google_count))
