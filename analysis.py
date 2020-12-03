2d8b1d82c2e191ac42c388828a8358d9373e7cc5
Carson Woods <cwoods289@gmail.com>
2020-12-03 17:41:21 +0000

    Try to extra p-value from analysis

--- analysis.py
+++ analysis.py
@@ -6,6 +6,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from scipy import stats
 
 
 #################
